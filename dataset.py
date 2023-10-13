d4f0efe62d37848789fe513c52c5a3fd77628064
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2023-10-13 10:19:39 +0300

    fix

--- dataset.py
+++ dataset.py
@@ -12,11 +12,11 @@ DATA_PREFIX = '/home/e.zamyatin/walk_gnn/data/'
 
 
 class EgoDataset(IterableDataset):
-    def __init__(self, ego_net_path):
+    def __init__(self, ego_net_path, limit=None):
         self.ego_net_path = ego_net_path
+        self.limit = limit
 
-    @staticmethod
-    def read_ego_net(ego_net_path, limit=None):
+    def read_ego_net(self, ego_net_path):
         cur_ego_id = -1
         cur_ego_net = None
         node_attr = None
@@ -26,7 +26,7 @@ class EgoDataset(IterableDataset):
         with open(ego_net_path, 'r') as ego_net_f:
             ego_net_f.readline()
             for ego_line in ego_net_f:
-                if limit is not None and n == limit:
+                if self.limit is not None and n == self.limit:
                     break
                 ego_line = ego_line.split(',')
                 ego_id, u, v, t, x1, x2, x3 = int(ego_line[0]), int(ego_line[1]), int(ego_line[2]), int(
@@ -56,7 +56,7 @@ class EgoDataset(IterableDataset):
                     cur_ego_net[0].append([u, v])
                     cur_ego_net[1].append([t, x1, x2, x3])
 
-            if len(cur_ego_net[0]) > 0 and cur_ego_id != -1 and (limit is None or n < limit):
+            if len(cur_ego_net[0]) > 0 and cur_ego_id != -1 and (self.limit is None or n < self.limit):
                 yield cur_ego_id, (node_attr[:was.sum()], np.array(cur_ego_net[0]).T, np.array(cur_ego_net[1]))
 
     def __iter__(self):
@@ -69,18 +69,18 @@ class EgoDataset(IterableDataset):
 
 
 class LabelDataset(IterableDataset):
-    def __init__(self, label_path):
+    def __init__(self, label_path, limit=None):
         self.label_path = label_path
+        self.limit = limit
 
-    @staticmethod
-    def read_label(label_path, limit=None):
+    def read_label(self, label_path):
         n = 0
         with open(label_path, 'r') as label_f:
             label_f.readline()
             cur_ego_id = -1
             cur_label = None
             for line in label_f:
-                if limit is not None and n == limit:
+                if self.limit is not None and n == self.limit:
                     break
                 ego_id, u, v = list(map(int, line.split(",")))
                 if ego_id != cur_ego_id:
@@ -89,7 +89,7 @@ class LabelDataset(IterableDataset):
                         n += 1
                     cur_ego_id, cur_label = ego_id, []
                 cur_label.append([u, v])
-            if cur_ego_id != -1 and len(cur_label) > 0 and limit is not None and n < limit:
+            if cur_ego_id != -1 and len(cur_label) > 0 and self.limit is not None and n < self.limit:
                 yield cur_ego_id, np.array(cur_label)
 
     def __iter__(self):
@@ -100,13 +100,14 @@ class LabelDataset(IterableDataset):
 
 
 class EgoLabelDataset(IterableDataset):
-    def __init__(self, ego_net_path, label_path):
+    def __init__(self, ego_net_path, label_path, limit=None):
         self.ego_net_path = ego_net_path
         self.label_path = label_path
+        self.limit = limit
 
     def __iter__(self):
-        ego_iter = EgoDataset(self.ego_net_path)
-        label_iter = LabelDataset(self.label_path)
+        ego_iter = EgoDataset(self.ego_net_path, self.limit)
+        label_iter = LabelDataset(self.label_path, self.limit)
         for (ego_id1, ego_f, f, edge_index), (ego_id2, label) in zip(ego_iter, label_iter):
             assert ego_id1 == ego_id2
             yield ego_f, f, edge_index, label
