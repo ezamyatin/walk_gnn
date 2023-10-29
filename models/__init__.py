8791ec79942aef9ec92c8ab012c36bcfae8a7cd5
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2023-10-29 14:44:36 +0300

    rgine

--- models/__init__.py
+++ models/__init__.py
@@ -19,6 +19,8 @@ def get_model(args):
         model = WalkGNN(node_dim=None, edge_dim=None, hid_dim=8, num_blocks=6, mlp_layers=2)
     elif args.model == 'gine':
         model = GINEModel(node_dim=8, edge_dim=4, hid_dim=256, num_blocks=6)
+    elif args.model == 'gine':
+        model = GINEModel(node_dim=8, edge_dim=4, hid_dim=256, num_blocks=6, r_version=True)
     elif args.model == 'gine_ohe':
         model = GINEModel(node_dim=8, edge_dim=4, hid_dim=256, num_blocks=6, use_degree_ohe=True, max_nodes=300)
     elif args.model == 'gin_ohe':
