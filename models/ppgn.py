f9cb7ed8390ac0cedc017fc6ca51c84c27a9c388
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2023-11-03 16:41:56 +0300

    fix

--- models/ppgn.py
+++ models/ppgn.py
@@ -103,33 +103,6 @@ def _init_weights(layer):
 
 
 
-def diag_offdiag_maxpool(input):
-    N = input.shape[-1]
-
-    max_diag = torch.max(torch.diagonal(input, dim1=-2, dim2=-1), dim=2)[0]  # BxS
-
-    # with torch.no_grad():
-    max_val = torch.max(max_diag)
-    min_val = torch.max(-1 * input)
-    val = torch.abs(torch.add(max_val, min_val))
-
-    min_mat = torch.mul(val, torch.eye(N, device=input.device)).view(1, 1, N, N)
-
-    max_offdiag = torch.max(torch.max(input - min_mat, dim=3)[0], dim=2)[0]  # BxS
-
-    return torch.cat((max_diag, max_offdiag), dim=1)  # output Bx2S
-
-
-def diag_offdiag_meanpool(input, level='graph'):
-    N = input.shape[-1]
-    if level == 'graph':
-        mean_diag = torch.mean(torch.diagonal(input, dim1=-2, dim2=-1), dim=2)  # BxS
-        mean_offdiag = (torch.sum(input, dim=[-1, -2]) - mean_diag * N) / (N * N - N)
-    else:
-        mean_diag = torch.diagonal(input, dim1=-2, dim2=-1)
-        mean_offdiag = (torch.sum(input, dim=-1) + torch.sum(input, dim=-2) - 2 * mean_diag) # / (2 * N - 2)
-    return torch.cat((mean_diag, mean_offdiag), dim=1)  # output Bx2S
-
 class PPGN(torch.nn.Module):
     # Provably powerful graph networks
     def __init__(self, emb_dim=64, use_embedding=False, use_spd=False, y_ndim=1,
@@ -148,18 +121,12 @@ class PPGN(torch.nn.Module):
         # ppgn modules
         num_blocks = 2
         num_rb_layers = 4
-        num_fc_layers = 2
 
         self.ppgn_rb = torch.nn.ModuleList()
         self.ppgn_rb.append(RegularBlock(num_blocks, initial_dim, emb_dim))
         for i in range(num_rb_layers - 1):
             self.ppgn_rb.append(RegularBlock(num_blocks, emb_dim, emb_dim))
 
-        self.ppgn_fc = torch.nn.ModuleList()
-        self.ppgn_fc.append(FullyConnected(emb_dim * 2, emb_dim))
-        for i in range(num_fc_layers - 2):
-            self.ppgn_fc.append(FullyConnected(emb_dim, emb_dim))
-        self.ppgn_fc.append(FullyConnected(emb_dim, 1, activation_fn=None))
         self.out_mlp = MLP(
             [emb_dim, emb_dim, 1],
             act='relu',
