4ee8f2d77997edbb782f38fda724239423d12617
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2023-11-04 10:03:40 +0300

    rm old ppgn

--- models/ppgn.py
+++ models/ppgn.py
@@ -1,9 +1,7 @@
-# code taken from https://github.com/GraphPKU/I2GNN/blob/master/count_models.py#L460
-
-import torch.nn as nn
-from torch_geometric.utils import dropout_adj, to_dense_adj, to_dense_batch, degree
+# code taken from https://github.com/hadarser/ProvablyPowerfulGraphNetworks_torch
 import torch
-from torch_geometric.nn.models import MLP
+import torch.nn as nn
+from torch_geometric.nn import MLP
 
 from models.gin import SimpleNormLayer
 
@@ -18,8 +16,8 @@ class RegularBlock(nn.Module):
         super().__init__()
 
         self.out_features = out_features
-        self.mlp1 = MlpBlock(in_features, out_features, depth_of_mlp)
-        self.mlp2 = MlpBlock(in_features, out_features, depth_of_mlp)
+        self.mlp1 = MlpBlock(in_features, out_features, depth_of_mlp + 1)
+        self.mlp2 = MlpBlock(in_features, out_features, depth_of_mlp + 1)
 
         self.skip = SkipConnection(in_features+out_features, out_features)
 
@@ -28,7 +26,7 @@ class RegularBlock(nn.Module):
         mlp2 = self.mlp2(inputs)
 
         mult = torch.matmul(mlp1, mlp2)
-        #mult = mult / self.out_features
+        mult /= self.out_features
         out = self.skip(in1=inputs, in2=mult)
         return out
 
@@ -109,77 +107,50 @@ def _init_weights(layer):
         nn.init.zeros_(layer.bias)
 
 
+class PPGN_V1(nn.Module):
+    def __init__(self, node_dim, edge_dim, hid_dim, num_blocks, mlp_layers, ignore_attr=False):
+        super().__init__()
+        self.edge_dim = edge_dim
+        self.node_dim = node_dim
+        self.ignore_attr = ignore_attr
+        if ignore_attr:
+            last_layer_features = 1
+        else:
+            last_layer_features = edge_dim + node_dim
 
-class PPGN(torch.nn.Module):
-    # Provably powerful graph networks
-    def __init__(self, emb_dim=64, use_spd=False, y_ndim=1,
-                 **kwargs):
-        super(PPGN, self).__init__()
-
-        self.use_spd = use_spd
-        self.y_ndim = y_ndim
-
-        initial_dim = 2
-
-        if self.use_spd:
-            initial_dim += 1
-
-        # ppgn modules
-        num_blocks = 2
-        num_rb_layers = 4
-
-        self.ppgn_rb = torch.nn.ModuleList()
-        self.ppgn_rb.append(RegularBlock(num_blocks, initial_dim, emb_dim))
-        for i in range(num_rb_layers - 1):
-            self.ppgn_rb.append(RegularBlock(num_blocks, emb_dim, emb_dim))
+        self.reg_blocks = nn.ModuleList()
+        for _ in range(num_blocks):
+            mlp_block = RegularBlock(mlp_layers, last_layer_features, hid_dim)
+            self.reg_blocks.append(mlp_block)
+            last_layer_features = hid_dim
 
         self.out_mlp = MLP(
-            [emb_dim, emb_dim, 1],
+            [last_layer_features, hid_dim, 1],
             act='relu',
             act_first=False,
-            norm=SimpleNormLayer(emb_dim),
+            norm=SimpleNormLayer(hid_dim),
             norm_kwargs=None
         )
 
-    def forward(self, x, edge_index):
-        # prepare dense data
-        device = edge_index.device
-        node_embedding = torch.zeros(x.shape[0]).to(device)
-        dense_edge_data = to_dense_adj(edge_index, max_num_nodes=x.shape[0])
-        dense_edge_data = torch.unsqueeze(dense_edge_data, -1)
-        dense_node_data, mask = to_dense_batch(node_embedding)
-        dense_node_data = torch.unsqueeze(dense_node_data, -1)
-        shape = dense_node_data.shape
-        shape = (shape[0], shape[1], shape[1], shape[2])
-        diag_node_data = torch.empty(*shape).to(device)
-
-        if self.use_spd:
-            dense_dist_mat = torch.zeros(shape[0], shape[1], shape[1], 1).to(device)
-
-        for g in range(shape[0]):
-            for i in range(shape[-1]):
-                diag_node_data[g, :, :, i] = torch.diag(dense_node_data[g, :, i])
-
-        if self.use_spd:
-            z = torch.cat([dense_dist_mat, dense_edge_data, diag_node_data], -1)
+    def forward(self, feat, edge_index, edge_attr):
+        n = feat.shape[0]
+        if self.ignore_attr:
+            x = torch.zeros((n, n, 1), device=feat.device, dtype=torch.float32)
+            x[edge_index[0], edge_index[1]] = 1
+            x = x.permute((2, 0, 1)).unsqueeze(0)
         else:
-            z = torch.cat([dense_edge_data, diag_node_data], -1)
-        z = torch.transpose(z, 1, 3)
+            fmtr = torch.zeros((n, n, self.edge_dim), device=feat.device, dtype=torch.float32)
+            fmtr[edge_index[0], edge_index[1]] = edge_attr
 
-        # ppng
-        for rb in self.ppgn_rb:
-            z = rb(z)
-        return self.out_mlp(z[0].permute((1, 2, 0))).reshape((x.shape[0], x.shape[0]))
+            nmtr = torch.zeros((n, n, self.node_dim), device=feat.device, dtype=torch.float32)
+            nmtr[torch.arange(n), torch.arange(n)] = feat
 
+            x = torch.cat((fmtr, nmtr), dim=-1).permute((2, 0, 1)).unsqueeze(0)
 
-class PPGNModel(nn.Module):
-    def __init__(self, hid_dim):
-        super().__init__()
-        self.model = PPGN(hid_dim)
+        for i, block in enumerate(self.reg_blocks):
+            x = block(x)
+        return self.out_mlp(x[0].permute((1, 2, 0))).reshape((feat.shape[0], feat.shape[0]))
 
-    def forward(self, x, edge_index, edge_attr):
-        return self.model.forward(x, edge_index)
-
-    def predict(self, x, edge_index, edge_attr):
-        return self.forward(x, edge_index, edge_attr)
+    def predict(self, feat, edge_index, edge_attr):
+        return self.forward(feat, edge_index, edge_attr)
 
